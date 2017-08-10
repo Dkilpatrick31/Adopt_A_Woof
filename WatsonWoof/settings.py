63756dc99c1fa3d9ea4793fa092f0c393a78abdb
Dane Kilpatrick <danekilpatrick@danes-air.attlocal.net>
2017-08-09 21:48:45 -0500

    Deployed to Heroku

--- WatsonWoof/settings.py
+++ WatsonWoof/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# 
+# # Simplified static file serving.
+# # https://warehouse.python.org/project/whitenoise/
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
